0f44bb327fd4548440cb696958681838af39d90c
nichecelis <nichecelis@gmail.com>
2026-03-30 18:36:14 -0500

    ajustes CI/CD

---
+++ backend/eslint.config.js
@@ -0,0 +1,30 @@
+const tsParser = require('@typescript-eslint/parser');
+const tseslint = require('@typescript-eslint/eslint-plugin');
+
+module.exports = [
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module',
+      },
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'no-console': 'warn',
+    },
+    ignores: ['dist/**', 'node_modules/**', 'tests/**', 'prisma/seed.ts'],
+  },
+];
